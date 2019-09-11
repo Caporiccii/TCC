5c48248847c04a8be022f8e4891ff8cbff55af95
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-09-11 16:55:39 -0300

    Projeto

---
+++ MiniProjeto2/src/FinancaDto.java
@@ -0,0 +1,9 @@
+public class FinancaDto  {
+
+    double computadorCusto = 1000;
+    double salaCusto = 2000;
+    double windowsCusto = 500;
+    double linuxCusto = 200;
+    double soma;
+    
+}
